fb44b26c8c35486ad96e3f469b4276238f0201e5
konrad920 <konradsowizral920@gmail.com>
2023-03-25 13:53:45 +0100

    Zadanie z analiza ciagu znakow

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write something: ");
+string text = Console.ReadLine();
+text = text.ToLower();
+
+Dictionary<char, int> counters = new Dictionary<char, int>();
+
+foreach (char charInText in text)
+{
+    counters.TryGetValue(charInText, out int value);
+    counters[charInText] = value + 1;
+}
+
+foreach (KeyValuePair<char, int> keyValuePair in counters)
+{
+    Console.WriteLine(keyValuePair);
+}
