b08f46f6d326e3faa1b90f05a0431056bc05cf92
konrad920 <konradsowizral920@gmail.com>
2023-04-28 23:55:24 +0200

    farenhait to celcius

---
+++ Exercises/Exercises/DegreeCounter.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercises
+{
+    internal class DegreeCounter
+    {
+        public float Degree { get; private set; }
+        public void CelToFar(float degree)
+        {
+            this.Degree = (degree * 9 / 5) + 32;
+        }
+
+        public void CelToFar(string degree)
+        {
+            if (float.TryParse(degree, out float result))
+            {
+                CelToFar(result);
+            }
+            else
+            {
+                throw new Exception("This is not float");
+            }
+        }
+
+        public void FarToCel(float degree)
+        {
+            this.Degree = (degree - 32) * 5 / 9;
+        }
+
+        public void FarToCel(string degree)
+        {
+            if (float.TryParse(degree, out float result))
+            {
+                FarToCel(result);
+            }
+            else
+            {
+                throw new Exception("This is not float");
+            }
+        }
+    }
+}
--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -1,16 +1,50 @@
-﻿Console.WriteLine("Write something: ");
-string text = Console.ReadLine();
-text = text.ToLower();
+﻿//Console.WriteLine("Write something: ");
+//string text = Console.ReadLine();
+//text = text.ToLower();
 
-Dictionary<char, int> counters = new Dictionary<char, int>();
+//Dictionary<char, int> counters = new Dictionary<char, int>();
 
-foreach (char charInText in text)
+//foreach (char charInText in text)
+//{
+//    counters.TryGetValue(charInText, out int value);
+//    counters[charInText] = value + 1;
+//}
+
+//foreach (KeyValuePair<char, int> keyValuePair in counters)
+//{
+//    Console.WriteLine(keyValuePair);
+//}
+
+using Exercises;
+
+Console.WriteLine("FarToCel [F] or CelToFar [C]");
+var userAnswer = Console.ReadLine();
+var degree = new DegreeCounter();
+if (userAnswer == "f" || userAnswer == "F")
 {
-    counters.TryGetValue(charInText, out int value);
-    counters[charInText] = value + 1;
+    Console.WriteLine("How much Farenhait degree you want convert to Celcius?: ");
+    var degreeFromUser = Console.ReadLine();
+    try
+    {
+        degree.FarToCel(degreeFromUser);
+    }
+    catch (Exception e)
+    { 
+        Console.WriteLine(e.Message);
+    }
+    Console.WriteLine($"It is: {degree.Degree} Celcius degree");
 }
-
-foreach (KeyValuePair<char, int> keyValuePair in counters)
+else if (userAnswer == "c" || userAnswer == "C")
 {
-    Console.WriteLine(keyValuePair);
+    Console.WriteLine("How much Celcius degree you want convert to Farenhait?: ");
+    var degreeFromUser = Console.ReadLine();
+    try
+    {
+        degree.CelToFar(degreeFromUser);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    Console.WriteLine($"It is: {degree.Degree} Farenhait degree");
 }
